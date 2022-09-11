ec54615e77bf0f5cb645d52be774be4b89e64997
Anna Gutsenko <a.gucenko@yandex.ru>
2022-09-11 10:50:31 +0300

    меню без depreceted

--- app/src/main/java/com/udacity/shoestore/screens/ListProductsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/ListProductsFragment.kt
@@ -2,16 +2,19 @@ package com.udacity.shoestore.screens
 
 import android.os.Bundle
 import android.view.*
-import android.widget.TextView
+import android.widget.FrameLayout
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragListOfProductsBinding
 import com.udacity.shoestore.databinding.ItemProductBinding
 import com.udacity.shoestore.models.ShoeViewModel
+
 
 class ListProductsFragment : Fragment(R.layout.frag_list_of_products) {
     lateinit var binding: FragListOfProductsBinding
@@ -27,36 +30,41 @@ class ListProductsFragment : Fragment(R.layout.frag_list_of_products) {
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listProductsFragment_to_itemShoeFragment)
         }
-        setHasOptionsMenu(true)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        sharedViewModel.shoe.observe(viewLifecycleOwner, Observer { item ->
+        sharedViewModel.shoe.observe(viewLifecycleOwner) { item ->
             val linearLayout = binding.layoutContainer
+            val ll1 = FrameLayout(requireContext())
+
             val itemProductBinding = DataBindingUtil.inflate<ItemProductBinding>(
                 layoutInflater,
                 R.layout.item_product,
-                linearLayout,
-                true
+                ll1,
+                false
             )
             itemProductBinding.shoe = item
-        })
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.logout -> {
-                findNavController().navigate(R.id.loginFragment)
-            }
+            linearLayout.addView(itemProductBinding.root)
         }
-        return true
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                return when (menuItem.itemId) {
+                    R.id.logout -> {
+                        findNavController().navigate(R.id.loginFragment)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
 }
